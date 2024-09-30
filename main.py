b33a4817c28c4e0a484499eacc995579a072f3b6
Adonis Georgiou <adonisgeorgiou@hotmail.co.uk>
2024-09-30 17:43:56 +0100

    Refactored code for player and cpu to select a pokemon

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
 import requests
 import json
+import random
 
 # Get the list of pokemon from the API
-url = 'https://pokeapi.co/api/v2/pokemon/'
-response = requests.get(url)
-pokemon_list = json.loads(response.text)['results']
+def get_pokemon_list(url):
+    response = requests.get(url)
+    return json.loads(response.text)['results']
+
+# Get the pokemon's data from the API
+def get_pokemon_data(url, pokemon):
+    url = "{}/{}/".format(url,pokemon)
+    response = requests.get(url)
+    return json.loads(response.text)
+
+
+def create_pokemon_data(data):
+    # Format height and weight properly
+    height = int(data['height'])
+    weight = int(data['weight'])
+
+    height_formatted = height / 10
+    weight_formatted = weight / 10
+
+    return {
+        "name": data["name"],
+        "ability": data['abilities'][0]["ability"],
+        "height": height_formatted,
+        "weight": weight_formatted,
+        "hp": data['stats'][0]["base_stat"],
+        "attack": data['stats'][1]["base_stat"],
+        "defence": data['stats'][2]["base_stat"],
+        "speed": data['stats'][5]["base_stat"],
+            }
+
+
+pokemon_list = get_pokemon_list('https://pokeapi.co/api/v2/pokemon/')
 
 for pokemon in pokemon_list:
     print(pokemon['name'])
 
 # Ask the user to choose a pokemon
-print('Enter your pokemon:')
+player_pokemon_choice = input('Enter your pokemon: ').lower()
 
-# Get the user's choice
-choice = input().lower()
-
-# Get the pokemon's data from the API
-url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(choice)
-response = requests.get(url)
-pokemon_data = json.loads(response.text)
-
-# to get ability
-abilities = pokemon_data['abilities'][0]
-ability = abilities['ability']
-
-# to format height and weight properly
-height = int(pokemon_data['height'])
-weight = int(pokemon_data['weight'])
-
-height_formatted = height / 10
-weight_formatted = weight / 10
+player_pokemon_data = get_pokemon_data('https://pokeapi.co/api/v2/pokemon/', player_pokemon_choice)
+player_pokemon_data_obj = create_pokemon_data(player_pokemon_data)
 
 # Print the pokemon's data
-print('Name: {}'.format(pokemon_data['name']))
-print('Weight: {}'.format(weight_formatted) + "(kgs)")
-print('Height: {}'.format(height_formatted) + "(m)")
-print('Ability: {}'.format(ability['name']))
+for (key, value) in player_pokemon_data_obj.items():
+    print(f"{key.capitalize()}: {value}")
+
+cpu_pokemon_choice = random.choice(pokemon_list)["name"]
+print(cpu_pokemon_choice)
+cpu_pokemon_data = get_pokemon_data('https://pokeapi.co/api/v2/pokemon/', cpu_pokemon_choice)
+print(f"CPU's Pokémon: {cpu_pokemon_data['name']}")
+
+# user_attack_random = random.randint(1, 101)
+# user_defense_random= random.randint(1, 101)
+# opponent_attack_random = random.randint(1, 101)
+# opponent_defense_random = random.randint(1, 101)
